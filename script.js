40c369a1343928ded23917d08a872f764705d6fe
Learningitnow <70935742+Learningitnow@users.noreply.github.com>
2020-10-01 18:36:51 -0400

    project 1, thrusday updates

--- script.js
+++ script.js
@@ -1,32 +1,99 @@
 // var basketball = {
-//     "async": true,
-// 	"crossDomain": true,
-// 	// "url": "https://api-nba-v1.p.rapidapi.com/players/lastName/",
-// 	"method": "GET",
-// 	"headers": {
-//         "x-rapidapi-host": "api-nba-v1.p.rapidapi.com",
-// 		"x-rapidapi-key": "b5a492aa05msh6869ac7f6671576p14fe03jsn3366a114fc88"
-// 	}
-// }
-var ApiKey = "b5a492aa05msh6869ac7f6671576p14fe03jsn3366a114fc88"
-var personName = $("#searchBox").value
-var queryUrl = `https://api-nba-v1.p.rapidapi.com/players/lastName/ + ${personName} + ApiKey`
-function searchApi() {
-    event.preventDefault()
+    //     "async": true,
+    // 	"crossDomain": true,
+    // 	// "url": "https://api-nba-v1.p.rapidapi.com/players/lastName/",
+    // 	"method": "GET",
+    // 	"headers": {
+        //         "x-rapidapi-host": "api-nba-v1.p.rapidapi.com",
+        // 		"x-rapidapi-key": "b5a492aa05msh6869ac7f6671576p14fe03jsn3366a114fc88"
+        // 	}
+        // }
+function changeSlide(){
+    
+    document.querySelector('.searchPage').classList.remove('active-slide')
+    document.querySelector('.searchPage').classList.add('hide')
+    document.querySelector('.searchResults').classList.add('active-slide')
+    document.querySelector('.searchResults').classList.remove('hide')
+    
+    searchApi2()
+    
+}
+function searchApi() { 
+    var personName = document.querySelector("#searchBox").value
+    console.log(personName)
+
     $.ajax({
-        url: queryUrl,
-        method: "GET"
+        url: `https://api-nba-v1.p.rapidapi.com/players/lastName/${personName}`,
+        crossDomain: true,
+        method: "GET",
+        headers: {
+            "x-rapidapi-host": "api-nba-v1.p.rapidapi.com",
+            "x-rapidapi-key": "b5a492aa05msh6869ac7f6671576p14fe03jsn3366a114fc88"
+        }
     })
     .then(function(response) {
 
         console.log(response);
-        var s = response.api.players[0]
-        console.log(s.heightInMeters)
-        document.querySelector(".card-text").textContent = `Height: ${s.heightInMeters}m`
+        var ballInfo = response.api.players[0]
+        console.log(ballInfo.heightInMeters)
+        document.querySelector("#name").innerHTML = `<h3>Full Name: ${ballInfo.firstName} ${ballInfo.lastName}</h3>`
+        document.querySelector(".info").innerHTML = 
+        `<p>Date of Birth: ${ballInfo.dateOfBirth}</p>
+        <p>Height: ${ballInfo.heightInMeters}m</p>
+        <p>Weight: ${ballInfo.weightInKilograms}kg</p>
+        <p>College name: ${ballInfo.collegeName}</p>
+        <p>Country: ${ballInfo.country}</p>
+        <p>Years pro: ${ballInfo.yearsPro}</p>
+        <p>Jerser number: ${ballInfo.leagues.standard.jersey}</p>
+        <p>Position: ${ballInfo.leagues.standard.pos}</p>
+        `
         
     })
-
-$(document).ready(function() {
-    document.querySelector("#searchBtn").addEventListener("click", searchApi)
-});
 }
+//var settings = {
+	//"async": true,
+	//"crossDomain": true,
+	//"url": "https://football-pro.p.rapidapi.com/api/v2.0/players/search/Virgil%2520van%2520Dijk?tz=Europe%252FAmsterdam",
+	//"method": "GET",
+	//"headers": {
+        //"x-rapidapi-host": "football-pro.p.rapidapi.com",
+		//"x-rapidapi-key": "0b276ca072mshebd5f85a364591cp19a269jsn385a7112a304"
+        //}
+        //}
+function searchApi2(){
+
+    var personNameSoccer = document.querySelector("#soccerSearchBox").value
+    console.log(personNameSoccer)
+
+    $.ajax({
+        url: `https://football-pro.p.rapidapi.com/api/v2.0/players/search/${personNameSoccer}`,
+        crossDomain: true,
+        method: "GET",
+        headers: {
+            "x-rapidapi-host": "football-pro.p.rapidapi.com",
+            "x-rapidapi-key": "0b276ca072mshebd5f85a364591cp19a269jsn385a7112a304"
+        }
+    })
+    .then(function(response) {
+        console.log(response);
+        var soccerInfo = response.data[0]
+        console.log(soccerInfo.birthcountry)
+        document.querySelector("#soccerName").innerHTML = `<h3>Full Name: ${soccerInfo.display_name}</h3>`
+        document.querySelector(".soccerInfo").innerHTML = 
+        `<p>Date of Birth: ${soccerInfo.birthdate}</p>
+        <p>Height: ${soccerInfo.height}m</p>
+        <p>Weight: ${soccerInfo.weight}kg</p>
+        <p>Place of Birth: ${soccerInfo.birthplace}, ${soccerInfo.birthcountry}</p>
+        <p>Image: ${soccerInfo.image_path}</p>
+        `
+
+    })
+}``
+// $('#searchBtn').on('click', function(){
+//     console.log('button was clicked')
+//     $('searchPage').classList.remove('active-slide')
+//     $('searchPage').classList.add('hide')
+//     $('searchResults').classList.add('active-slide')
+//     $('searchPage').classList.remove('hide')
+// })
+
