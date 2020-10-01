87d993028973dcb60b99d3e2d58a7e7899530c9b
Dailey Kaze <daileykaze@gmail.com>
2020-10-01 12:28:18 -0400

    updating everything

--- script.js
+++ script.js
@@ -1,20 +1,32 @@
-var basketball = {
-    "async": true,
-	"crossDomain": true,
-	"url": "https://api-nba-v1.p.rapidapi.com/players/lastName/james",
-	"method": "GET",
-	"headers": {
-        "x-rapidapi-host": "api-nba-v1.p.rapidapi.com",
-		"x-rapidapi-key": "b5a492aa05msh6869ac7f6671576p14fe03jsn3366a114fc88"
-	}
-}
-document.querySelector(".searchBtn").addEventListener("click", searchApi)
+// var basketball = {
+//     "async": true,
+// 	"crossDomain": true,
+// 	// "url": "https://api-nba-v1.p.rapidapi.com/players/lastName/",
+// 	"method": "GET",
+// 	"headers": {
+//         "x-rapidapi-host": "api-nba-v1.p.rapidapi.com",
+// 		"x-rapidapi-key": "b5a492aa05msh6869ac7f6671576p14fe03jsn3366a114fc88"
+// 	}
+// }
+var ApiKey = "b5a492aa05msh6869ac7f6671576p14fe03jsn3366a114fc88"
+var personName = $("#searchBox").value
+var queryUrl = `https://api-nba-v1.p.rapidapi.com/players/lastName/ + ${personName} + ApiKey`
 function searchApi() {
-    $.ajax(basketball).then(function (response) {
+    event.preventDefault()
+    $.ajax({
+        url: queryUrl,
+        method: "GET"
+    })
+    .then(function(response) {
+
         console.log(response);
         var s = response.api.players[0]
         console.log(s.heightInMeters)
         document.querySelector(".card-text").textContent = `Height: ${s.heightInMeters}m`
         
-    });
+    })
+
+$(document).ready(function() {
+    document.querySelector("#searchBtn").addEventListener("click", searchApi)
+});
 }
